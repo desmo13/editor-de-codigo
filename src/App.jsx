0da2e5f1dfa890f3ebe68468ee63c95a6c6085b0
Mario Manuel <71732977+desmo13@users.noreply.github.com>
2025-10-27 13:33:24 +0100

    Add file opening functionality and enhance preview styling

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,54 @@ function App() {
   }
 
   const handlePreview = () => {
-    setPreview(code)
+      const styledCode = `
+  <html>
+    <head>
+      <style>
+        html, body {
+          background-color: rgba(55, 58, 66, 0.6);
+          color: white;
+          font-family: monospace;
+          margin: 0;
+          padding: 1rem;
+        }
+        a { color: #7c5cff; }
+      </style>
+    </head>
+    <body>${code}</body>
+  </html>`;
+    setPreview(styledCode)
   }
+const handleOpenFile = async() => {
+    if (!('showOpenFilePicker' in window)) {
+    alert('Tu navegador no soporta File System Access API');
+    return;
+  }
+  const [fileHandle] = await window.showOpenFilePicker({
+      multiple: false,
+      types: [
+        {
+          description: 'HTML/CSS/JS',
+          accept: {
+            'text/html': ['.html', '.htm'],
+            'text/css': ['.css'],
+            'text/javascript': ['.js'],
+            'text/plain': ['.txt'],
+          },
+        },
+      ],
+    });
 
+    const file = await fileHandle.getFile();
+    const fileText = await file.text();
+    setCode(fileText);
+  
+};
   return (
     <>
       <div className="topbar">
         <div className="brand">⚡ Mini HTML Preview</div>
+        <button className="btn" onClick={handleOpenFile}>Abrir</button>
         <button className="btn" onClick={handlePreview}>Preview</button>
       </div>
 
